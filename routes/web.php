69c749d91ba9a70fb47f239ce6e095d6638f5fa4
tegardimas-1 <badakull123@gmail.com>
2026-04-10 08:33:16 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/tentang', function () {
-    return '<h1>Ini adalah halaman Tentang Aplikasi Event Hub</h1>';
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/profil', function () {
+    return view('profil');
+});
+
+Route::get('/katalog', function () {
+    return view('katalog');
+});
+
+Route::get('/bantuan', function () {
+    return view('bantuan');
 });
 
 Route::get('/kontak', function () {
     return view('contact');
+});
+
+Route::get('/tentang', function () {
+    return '<h1>Ini adalah halaman Tentang Aplikasi Event Hub</h1>';
 });
