ed107bd04645b19ce731000721ff7e4f2e664da0
tegardimas-1 <badakull123@gmail.com>
2026-04-19 13:39:25 +0700

    Create EventController.php

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+    /**
+     * Show the event detail page
+     */
+    public function show($id)
+    {
+        // TODO: Ambil data event dari database berdasarkan ID
+        // Untuk sekarang, hanya render view tanpa data
+        return view('event-detail');
+    }
+
+    /**
+     * Show the checkout page for an event
+     */
+    public function checkout($id)
+    {
+        // TODO: Ambil data event dari database berdasarkan ID
+        // TODO: Ambil data tiket yang tersedia
+        // Untuk sekarang, hanya render view tanpa data
+        return view('checkout');
+    }
+
+    /**
+     * Show events catalog/listing
+     */
+    public function index()
+    {
+        // TODO: Ambil semua events dari database
+        // Untuk sekarang, hanya render view tanpa data
+        return view('katalog');
+    }
+}
