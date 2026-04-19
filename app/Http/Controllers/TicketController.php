705d48d25fe5f5f3282903504fe43db62e1bbbf6
tegardimas-1 <badakull123@gmail.com>
2026-04-19 13:39:34 +0700

    Create TicketController.php

---
+++ app/Http/Controllers/TicketController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TicketController extends Controller
+{
+    /**
+     * Show the ticket page
+     */
+    public function show($id)
+    {
+        // TODO: Ambil data tiket dari database berdasarkan ID
+        // Untuk sekarang, hanya render view tanpa data
+        return view('ticket');
+    }
+}
