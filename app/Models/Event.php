4ddf0745a19fd08c0a933d684b92273e0d9e5c5f
tegardimas-1 <badakull123@gmail.com>
2026-04-24 08:07:57 +0700

    Create Event.php

---
+++ app/Models/Event.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'title',
+        'description',
+        'date',
+        'location',
+        'price',
+        'stock',
+        'poster_path',
+    ];
+
+    protected $casts = [
+        'date' => 'datetime',
+        'price' => 'integer',
+        'stock' => 'integer',
+    ];
+
+    /**
+     * Get the category for the event.
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
+
