052eb531a38212571b63c4b1f9df902581991249
tegardimas-1 <badakull123@gmail.com>
2026-04-24 08:07:47 +0700

    Update EventController.php

--- app/Http/Controllers/Admin/EventController.php
+++ app/Http/Controllers/Admin/EventController.php
@@ -7,8 +7,74 @@ use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
+    /**
+     * Tampilkan daftar event
+     */
     public function index()
     {
-        return view('admin.events');
+        // TODO: Ganti dengan query ke database
+        $events = [
+            (object)[
+                'id' => 1,
+                'title' => 'Jazz Night 2024',
+                'category' => 'Musik',
+                'date' => '16 Nov 2024',
+                'price' => 'Rp 150.000',
+                'stock' => 100,
+                'sold' => 42
+            ],
+            (object)[
+                'id' => 2,
+                'title' => 'AI & Future Workshop',
+                'category' => 'Tech',
+                'date' => '26 Oct 2024',
+                'price' => 'Rp 50.000',
+                'stock' => 50,
+                'sold' => 12
+            ],
+        ];
+
+        return view('admin.events.index', compact('events'));
+    }
+
+    /**
+     * Tampilkan form create event
+     */
+    public function create()
+    {
+        return view('admin.events.create');
+    }
+
+    /**
+     * Simpan event baru
+     */
+    public function store(Request $request)
+    {
+        // TODO: Implementasi store
+    }
+
+    /**
+     * Tampilkan form edit event
+     */
+    public function edit($id)
+    {
+        return view('admin.events.edit');
+    }
+
+    /**
+     * Update event
+     */
+    public function update(Request $request, $id)
+    {
+        // TODO: Implementasi update
+    }
+
+    /**
+     * Hapus event
+     */
+    public function destroy($id)
+    {
+        // TODO: Implementasi destroy
     }
 }
+
