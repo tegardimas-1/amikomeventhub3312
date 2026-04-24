9835b855010d4f13056c3de07f0af44a211c5753
tegardimas-1 <badakull123@gmail.com>
2026-04-24 08:07:54 +0700

    Create Category.php

---
+++ app/Models/Category.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+    ];
+
+    /**
+     * Get the events for the category.
+     */
+    public function events()
+    {
+        return $this->hasMany(Event::class);
+    }
+}
+
