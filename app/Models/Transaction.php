aae9331a057dad5edd0514779d0273d7d3a043e0
tegardimas-1 <badakull123@gmail.com>
2026-04-24 08:08:01 +0700

    Create Transaction.php

---
+++ app/Models/Transaction.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'event_id',
+        'order_id',
+        'customer_name',
+        'customer_email',
+        'customer_phone',
+        'total_price',
+        'status',
+        'snap_token',
+    ];
+
+    protected $casts = [
+        'total_price' => 'integer',
+    ];
+
+    /**
+     * Get the event for the transaction.
+     */
+    public function event()
+    {
+        return $this->belongsTo(Event::class);
+    }
+}
+
